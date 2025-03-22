085c5b7f4fbe7bf1cb8fa0b45eb22169cf07de89
Andwele Ancheta <andwele.ancheta@gmail.com>
2025-03-23 01:44:26 +1100

    Fix deployment issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { Suspense } from 'react'; // Import Suspense
 import '@/app/globals.css';
 import Header from '@/components/header';
 
@@ -13,7 +14,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <SessionProvider>
-          <Header/>
+          {/* Wrap Header in Suspense with a fallback */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Header />
+          </Suspense>
           {children}
         </SessionProvider>
       </body>
